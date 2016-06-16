5244f3f728def180acfcbb734db242b8dfd00fe4
Arne Redlich <arne.redlich@googlemail.com>
2016-06-16 12:16:45 +0200

    Use the newly introduced libovsvolumedriver packages

--- jenkins/ubuntu/build_debs.sh
+++ jenkins/ubuntu/build_debs.sh
@@ -23,9 +23,9 @@ echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selecti
 
 sudo apt-get update -qq
 
-## we know we depend on the volumedriver-dev package, and we want latest greatest, so install from artifact
+## we know we depend on the libovsvolumedriver-dev package, and we want latest greatest, so install from artifact
 
-for p in volumedriver-base_*_amd64.deb volumedriver-server_*_amd64.deb volumedriver-dev_*_amd64.deb
+for p in libovsvolumedriver_*_amd64.deb libovsvolumedriver-dev_*_amd64.deb
 do
   echo -n "Installing $p..."
   sudo dpkg -i $p || sudo apt-get install --allow-unauthenticated -qq -y -f
